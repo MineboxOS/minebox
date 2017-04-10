53c10c7d81e4d84059525f34867083b81ec9dce2
Andreas Petersson <andreas@petersson.at>
2017-04-10 09:47:35 +0200

    MIN-89: SDNotify integration (missing file)

---
+++ minebd/src/main/java/io/minebox/nbd/SystemdUtil.java
@@ -0,0 +1,42 @@
+package io.minebox.nbd;
+
+import info.faljse.SDNotify.SDNotify;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class SystemdUtil {
+    private static final boolean hasEnv;
+    private final static Logger LOGGER = LoggerFactory.getLogger(SystemdUtil.class);
+
+    static {
+        final String notifySocket = System.getenv().get("NOTIFY_SOCKET");
+        hasEnv = !(notifySocket == null || notifySocket.length() == 0);
+        if (!hasEnv) {
+            LOGGER.info("we appear to run outside systemd");
+        } else {
+            LOGGER.info("we appear to run inside systemd");
+        }
+    }
+
+    static void sendStopping() {
+        LOGGER.info("sendStopping");
+        if (hasEnv) {
+            SDNotify.sendStopping();
+        }
+    }
+
+    static void sendError(int errno) {
+        LOGGER.info("sendErrno {}", errno);
+        if (hasEnv) {
+            SDNotify.sendErrno(errno);
+        }
+    }
+
+    static void sendNotify() {
+        LOGGER.info("sendNotify");
+        if (hasEnv) {
+            SDNotify.sendNotify();
+        }
+    }
+}
