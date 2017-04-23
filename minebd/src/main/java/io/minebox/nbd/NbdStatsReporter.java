42a180858e5dea6e87c373daf1301440c70a00d8
Andreas Petersson <andreas@petersson.at>
2017-04-24 00:06:39 +0200

    meter logging through slf4j (configurable in yaml)

--- minebd/src/main/java/io/minebox/nbd/NbdStatsReporter.java
+++ minebd/src/main/java/io/minebox/nbd/NbdStatsReporter.java
@@ -2,8 +2,9 @@ package io.minebox.nbd;
 
 import java.util.concurrent.TimeUnit;
 
-import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.ScheduledReporter;
+import com.codahale.metrics.Slf4jReporter;
 import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,13 +12,14 @@ import org.slf4j.LoggerFactory;
 /**
  * Created by andreas on 23.04.17.
  */
-
 public class NbdStatsReporter {
     private final static Logger LOGGER = LoggerFactory.getLogger(NbdStatsReporter.class);
 
     @Inject
     public NbdStatsReporter(MetricRegistry metrics) {
-        ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics)
+        ScheduledReporter reporter = Slf4jReporter.forRegistry(metrics)
+                .withLoggingLevel(Slf4jReporter.LoggingLevel.DEBUG)
+                .outputTo(LOGGER)
                 .convertRatesTo(TimeUnit.SECONDS)
                 .convertDurationsTo(TimeUnit.MILLISECONDS)
                 .build();
