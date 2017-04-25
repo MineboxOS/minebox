0bcf959541c546b5983c44e162426e0da10e1114
Andreas Petersson <andreas@petersson.at>
2017-04-25 14:24:15 +0200

    MIN-96: jmh to compare different hashing strategies (non-HW accel.)

--- minebd/src/test/java/io/minebox/nbd/encryption/BitPatternGeneratorTest.java
+++ minebd/src/test/java/io/minebox/nbd/encryption/BitPatternGeneratorTest.java
@@ -14,19 +14,19 @@ public class BitPatternGeneratorTest {
     @Test
     public void testSecureRandom() throws Exception { //sha-1 based
         final byte[] check = underTest.secureRandomHashing(50);
-//        Assert.assertEquals(Bytes.asList(new byte[]{-29, -41, 126, 90, 68}), Bytes.asList(check).subList(0, 5));
+        Assert.assertEquals(Bytes.asList(new byte[]{-29, -41, 126, 90, 68}), Bytes.asList(check).subList(0, 5));
     }
 
     @Test
     public void testGuava() throws Exception {
         final byte[] check = underTest.guavaHash(50);
-//        Assert.assertEquals(Bytes.asList(new byte[]{54, 86, -66, 18, 104}), Bytes.asList(check).subList(0, 5));
+        Assert.assertEquals(Bytes.asList(new byte[]{54, 86, -66, 18, 104}), Bytes.asList(check).subList(0, 5));
     }
 
     @Test
     public void testMD() throws Exception {
         final byte[] check = underTest.digestHash(50);
-//        Assert.assertEquals(Bytes.asList(new byte[]{54, 86, -66, 18, 104}), Bytes.asList(check).subList(0, 5));
+        Assert.assertEquals(Bytes.asList(new byte[]{54, 86, -66, 18, 104}), Bytes.asList(check).subList(0, 5));
     }
 }
 
