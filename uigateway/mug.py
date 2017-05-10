f1327394ad7b30de451498e123fbf6492fa4468d
Robert Kaiser <kairo@kairo.at>
2017-05-10 15:46:55 +0200

    imply a found backup by backupfiles being a list (and None otherwise), add relative size/precent (compared to previous backup)

--- uigateway/mug.py
+++ uigateway/mug.py
@@ -44,24 +44,34 @@ def api_backup_status(backupname):
     if not re.match(r'^\d+$', backupname):
         return jsonify(error="Illegal backup name."), 400
 
-    backupfiles, backup_found, is_finished = getBackupFiles(backupname)
-    if not backup_found:
+    backupfiles, is_finished = getBackupFiles(backupname)
+    if backupfiles is None:
         return jsonify(message="No backup known with that name."), 400
 
-    if backupfiles is None:
+    if len(backupfiles) < 1:
         # Before uploads are scheduled, we find a backup but no files.
         files = -1
         total_size = -1
+        rel_size = -1
         progress = 0
+        rel_progress = 0
         status = "PENDING"
         fully_available = False
     else:
+        backuplist = getBackupList()
+        currentidx = backuplist.index(backupname)
+        if currentidx > 0:
+            prev_backupfiles, prev_finished = getBackupFiles(backuplist[currentidx - 1])
+        else:
+            prev_backupfiles = None
         filedata, status_code = getFromSia('renter/files')
         if status_code == 200:
             # create a dict generated from the JSON response.
             files = 0
             total_size = 0
-            pct_size = 0
+            total_pct_size = 0
+            rel_size = 0
+            rel_pct_size = 0
             fully_available = True
             for file in filedata["files"]:
                 if file["siapath"] in backupfiles:
@@ -69,13 +79,20 @@ def api_backup_status(backupname):
                     # We may want to only report files not included in previous backups.
                     files += 1
                     total_size += file["filesize"]
-                    pct_size += file["filesize"] * file["uploadprogress"] / 100
+                    total_pct_size += file["filesize"] * file["uploadprogress"] / 100
+                    if prev_backupfiles is None or not file["siapath"] in prev_backupfiles:
+                        rel_size += file["filesize"]
+                        rel_pct_size += file["filesize"] * file["uploadprogress"] / 100
                     if not file["available"]:
                         fully_available = False
-            progress = pct_size / total_size * 100 if total_size else 0
+            # If size is 0, we report 100% progress.
+            # This is really needed for relative as otherwise a backup with no
+            # difference to the previous would never go to 100%.
+            progress = total_pct_size / total_size * 100 if total_size else 100
+            rel_progress = rel_pct_size / rel_size * 100 if rel_size else 100
             if is_finished and fully_available:
                 status = "FINISHED"
-            elif pct_size:
+            elif total_pct_size:
                 status = "UPLOADING"
             else:
                 status = "PENDING"
@@ -83,16 +100,20 @@ def api_backup_status(backupname):
             app.logger.error("Error %s getting Sia files: %s" % (status_code, str(filedata)))
             files = -1
             total_size = -1
+            rel_size = -1
             progress = 0
+            rel_progress = 0
             status = "ERROR"
             fully_available = False
 
     return jsonify(
       progress=progress,
+      relative_progress=rel_progress,
       status=status,
       metadata="TBD",
       numFiles=files,
-      size=total_size
+      size=total_size,
+      relative_size=rel_size
     )
 
 
@@ -193,32 +214,33 @@ def checkLogin():
 
 
 def getBackupList():
-    return [re.sub(r'.*backup\.(\d+)(\.zip)?', r'\1', f)
-            for f in glob(join(METADATA_BASE, "backup.*"))
-              if (isfile(f) and f.endswith(".zip")) or
-                 isdir(f) ]
+    backuplist = [re.sub(r'.*backup\.(\d+)(\.zip)?', r'\1', f)
+                  for f in glob(join(METADATA_BASE, "backup.*"))
+                    if (isfile(f) and f.endswith(".zip")) or
+                       isdir(f) ]
+    backuplist.sort()
+    return backuplist
 
 
 def getBackupFiles(backupname):
     backupfiles = None
-    backup_found = False
     is_finished = None
     zipname = join(METADATA_BASE, "backup.%s.zip" % backupname)
     dirname = join(METADATA_BASE, "backup.%s" % backupname)
     if isfile(zipname):
-        backup_found = True
+        backupfiles = []
         is_finished = True
         with ZipFile(zipname, 'r') as backupzip:
             backupfiles = [re.sub(r'.*backup\.\d+\/(.*)\.sia$', r'\1', f)
                            for f in backupzip.namelist()]
     elif isdir(dirname):
-        backup_found = True
+        backupfiles = []
         is_finished = False
         flist = join(dirname, "files")
         if isfile(flist):
             with open(flist) as f:
                 backupfiles = [line.rstrip('\n') for line in f]
-    return backupfiles, backup_found, is_finished
+    return backupfiles, is_finished
 
 
 @app.errorhandler(404)
