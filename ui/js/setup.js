6ae19db542fab86392fb1d05c388c54f72cb28a8
Guillermo <guillermo@minebox.io>
2017-05-17 20:13:28 +0200

    Minebox setup UI: Loading page with a decent loading "spinner", checkboxes and layout.

--- ui/js/setup.js
+++ ui/js/setup.js
@@ -174,3 +174,80 @@ $('body').on('click', '#print-encryption-key-qr-code', function() {
 	register.hostname = $('#setup-page .register-section .register-hostname').val()
 	register.seed = $('#encryption-key-string').val();
 });
+
+
+
+(function loadingSpace() {
+	var $loadingSpace = $('#loading-space');
+	var $stars;
+
+	function generateStars() {
+		var count = getRandomInt(10,20);
+		for ( var n = 0; n < count; n++ ) {
+			$loadingSpace.append( '<span class="star" id="' + getRandomString(10) + '"></span>' );
+		}
+		$stars = $('.star');
+	}
+
+	function init() {
+		generateStars();
+		for ( var n = 0; n < $stars.length; n++ ) {
+			animateStars( $($stars[n]) );
+		}
+	}
+
+	function animateStars( $element ) {
+		var props = {};
+		var timeout;
+		var choice;
+		//invert direction
+		if ( $element.attr('data-position') == 'left' ) {
+			//move to right
+			props.left = '1000%';
+			$element.attr('data-position', 'right');
+		} else if ( $element.attr('data-position') == 'right' ) {
+			//move to left
+			props.left = '-1000%';
+			$element.attr('data-position', 'left');
+		} else {
+			//left has not been set yet, moving to random
+			choice = getRandomInt(0,1);
+
+			if ( !choice ) {
+				//move to left
+				props.left = '-1000%';
+				$element.attr('data-position', 'left');
+			} else {
+				//move to right
+				props.left = '1000%';
+				$element.attr('data-position', 'right');
+			}
+		}
+
+		props.top = getRandomInt(10, 90) + '%';
+		props.width = getRandomInt(80,200);
+		props.height = props.width / getRandomInt(20,30); //a fraction of its width so it is stylized horizontally
+		props.duration = getRandomInt(8,50) / 10;
+
+		$element.css({
+			'width': props.width,
+			'height': props.height,
+			'top': props.top,
+			'transform': 'translateX(' + props.left + ')',
+			'transition': 'transform ' + props.duration + 's ease, margin-left ' + props.duration + 's ease'
+		});
+
+		timeout = setTimeout(function() {
+			reanimateStars( $element );
+		}, (props.duration * 1000) + 10);
+
+	}
+
+	function reanimateStars( $element ) {
+		animateStars($element);
+	}
+
+
+	init();
+
+})();
