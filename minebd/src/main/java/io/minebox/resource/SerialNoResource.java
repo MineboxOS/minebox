d35a34bf0db7369d8a48e87185082e9342726314
Andreas Petersson <andreas@petersson.at>
2017-05-22 17:45:02 +0200

    less error logging on getPubIdentifier

--- minebd/src/main/java/io/minebox/resource/SerialNoResource.java
+++ minebd/src/main/java/io/minebox/resource/SerialNoResource.java
@@ -2,16 +2,17 @@ package io.minebox.resource;
 
 import javax.annotation.security.PermitAll;
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import com.google.common.util.concurrent.ListenableFuture;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import io.minebox.nbd.Encryption;
+import io.minebox.nbd.encryption.EncyptionKeyProvider;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,19 +24,29 @@ public class SerialNoResource {
     public static final String PATH = "/serialnumber";
     private static final Logger LOGGER = LoggerFactory.getLogger(SerialNoResource.class);
     private final Encryption encryption;
+    private final EncyptionKeyProvider encyptionKeyProvider;
 
 
     @Inject
-    public SerialNoResource(Encryption encryption) {
+    public SerialNoResource(Encryption encryption, EncyptionKeyProvider encyptionKeyProvider) {
         this.encryption = encryption;
+        this.encyptionKeyProvider = encyptionKeyProvider;
     }
 
     @GET
     @Path("/")
     @Produces("text/plain")
     @PermitAll
-    public String getSerialNumber() {
-        return encryption.getPublicIdentifier();
+    public Response getSerialNumber() {
+        final ListenableFuture<String> masterPassword = encyptionKeyProvider.getMasterPassword();
+        if (!masterPassword.isDone()) {
+            return Response.status(Response.Status.PRECONDITION_FAILED)
+                    .entity("Key is not set yet..")
+                    .build();
+        }
+        return Response
+                .ok(encryption.getPublicIdentifier())
+                .build();
     }
 
 }
