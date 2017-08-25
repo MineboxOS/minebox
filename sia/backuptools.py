35408b142f65d4b2d643e3fb4e481c8b5ad66e0a
Robert Kaiser <kairo@kairo.at>
2017-08-25 22:45:58 +0200

    count files with too little redundancy as 'in progress' instead of done, put redundancy limit into a constant, break some long lines

--- sia/backuptools.py
+++ sia/backuptools.py
@@ -19,6 +19,8 @@ from connecttools import (get_demo_url, get_from_sia, post_to_sia,
 from backupinfo import *
 from siatools import check_sia_sync, SIA_DIR
 
+REDUNDANCY_LIMIT = 2.0
+
 def check_backup_prerequisites():
     # Check if prerequisites are met to make backups.
     success, errmsg = check_sia_sync()
@@ -121,21 +123,32 @@ def initiate_uploads(status):
             (froot, fext) = path.splitext(filename)
             sia_fname = '%s.%s%s' % (froot, int(fileinfo.st_mtime), fext)
             status["backupfiles"].append(sia_fname)
-            if siafiles and any(sf['siapath'] == sia_fname and sf['available'] for sf in siafiles):
-                current_app.logger.info("%s is part of the set and already uploaded." % sia_fname)
-            elif siafiles and any(sf['siapath'] == sia_fname for sf in siafiles):
+            if (siafiles
+                and any(sf["siapath"] == sia_fname
+                        and sf["available"]
+                        and sf["redundancy"] > REDUNDANCY_LIMIT
+                        for sf in siafiles)):
+                current_app.logger.info("%s is part of the set and already uploaded."
+                                        % sia_fname)
+            elif (siafiles
+                  and any(sf["siapath"] == sia_fname
+                          for sf in siafiles)):
                 status["uploadsize"] += fileinfo.st_size
                 status["uploadfiles"].append(sia_fname)
-                current_app.logger.info("%s is part of the set and the upload is already in progress." % sia_fname)
+                current_app.logger.info("%s is part of the set and the upload is already in progress."
+                                        % sia_fname)
             else:
                 status["uploadsize"] += fileinfo.st_size
                 status["uploadfiles"].append(sia_fname)
-                current_app.logger.info('%s has to be uploaded, starting that.' % sia_fname)
+                current_app.logger.info("%s has to be uploaded, starting that."
+                                        % sia_fname)
                 siadata, sia_status_code = post_to_sia('renter/upload/%s' % sia_fname,
                                                        {'source': filepath})
                 if sia_status_code != 204:
-                    return False, "ERROR: sia upload error %s: %s" % (sia_status_code, siadata['message'])
-            status["backupfileinfo"].append({"siapath": sia_fname, "size": fileinfo.st_size})
+                    return False, ("ERROR: sia upload error %s: %s"
+                                   % (sia_status_code, siadata["message"]))
+            status["backupfileinfo"].append({"siapath": sia_fname,
+                                             "size": fileinfo.st_size})
 
     with open(bfinfo_path, 'w') as outfile:
         json.dump(status["backupfileinfo"], outfile)
@@ -177,7 +190,7 @@ def wait_for_uploads(status):
             min_redundancy = min(redundancy) if redundancy else 0
             # Break if the backup is fully available on sia and has enough
             # minimum redundancy.
-            if fully_available and min_redundancy >= 2.0:
+            if fully_available and min_redundancy >= REDUNDANCY_LIMIT:
                 status["available"] = True
                 current_app.logger.info("Backup is fully available and minimum file redundancy is %.1f, we can finish things up."
                                         % min_redundancy)
