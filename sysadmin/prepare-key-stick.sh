ae6166be7c552e2d9abf6771e27213034ad1bae0
Robert Kaiser <kairo@kairo.at>
2017-10-04 19:56:53 +0200

    MIN-119: always force overwriting an existing filesystem as those sticks come with vfat already

--- sysadmin/prepare-key-stick.sh
+++ sysadmin/prepare-key-stick.sh
@@ -4,5 +4,5 @@ KEYDEVICE=${KEYDEVICE:-disk/by-id/usb-Generic_Flash_Disk_*-0\:0-part1}
 
 echo "Preparing Minebox Key USB Stick:"
 echo "Formatting to btrfs..."
-/usr/sbin/mkfs.btrfs /dev/${KEYDEVICE} --label MINEBOXKEY
+/usr/sbin/mkfs.btrfs --force /dev/${KEYDEVICE} --label MINEBOXKEY
 echo "Done, the USB stick should be ready to hold a Minebox key."
