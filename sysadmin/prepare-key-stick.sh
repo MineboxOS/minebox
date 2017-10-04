d00ddc34efaeaa176add51749c1e359a4684ed53
Robert Kaiser <kairo@kairo.at>
2017-10-04 19:52:40 +0200

    MIN-119: user btrfs instead of ext4 to work better with Rockstor integration

--- sysadmin/prepare-key-stick.sh
+++ sysadmin/prepare-key-stick.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
 
 KEYDEVICE=${KEYDEVICE:-disk/by-id/usb-Generic_Flash_Disk_*-0\:0-part1}
 
 echo "Preparing Minebox Key USB Stick:"
-echo "Formatting to ext4..."
-/usr/sbin/mkfs.ext4 /dev/${KEYDEVICE}
-echo "Setting label..."
-/usr/sbin/e2label /dev/${KEYDEVICE} MINEBOXKEY
+echo "Formatting to btrfs..."
+/usr/sbin/mkfs.btrfs /dev/${KEYDEVICE} --label MINEBOXKEY
 echo "Done, the USB stick should be ready to hold a Minebox key."
