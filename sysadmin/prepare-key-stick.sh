89d10987344b80e12b342d7033d947dec831a331
Robert Kaiser <kairo@kairo.at>
2017-10-04 17:01:33 +0200

    MIN-119: Parameterize the device name

--- sysadmin/prepare-key-stick.sh
+++ sysadmin/prepare-key-stick.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+KEYDEVICE=${KEYDEVICE:-disk/by-id/usb-Generic_Flash_Disk_*-0\:0-part1}
+
 echo "Preparing Minebox Key USB Stick:"
 echo "Formatting to ext4..."
-/usr/sbin/mkfs.ext4 /dev/disk/by-id/usb-Generic_Flash_Disk_*-0\:0-part1
+/usr/sbin/mkfs.ext4 /dev/${KEYDEVICE}
 echo "Setting label..."
-/usr/sbin/e2label /dev/disk/by-id/usb-Generic_Flash_Disk_*-0\:0-part1 MINEBOXKEY
+/usr/sbin/e2label /dev/${KEYDEVICE} MINEBOXKEY
 echo "Done, the USB stick should be ready to hold a Minebox key."
